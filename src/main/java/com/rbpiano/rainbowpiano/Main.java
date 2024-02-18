703d591467e7eba8cd154e902f5598fba85b703a
zzhongli <zl.zhonli@gmail.com>
2024-02-18 22:37:24 +0800

    -

--- src/main/java/com/rbpiano/rainbowpiano/Main.java
+++ src/main/java/com/rbpiano/rainbowpiano/Main.java
@@ -4,6 +4,7 @@ package com.rbpiano.rainbowpiano;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,9 +14,10 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("MainScreen.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Rainbow Piano");
         stage.setScene(scene);
         stage.setResizable(false);
+        stage.getIcons().add(new Image("icon.png"));
         stage.show();
     }
 
